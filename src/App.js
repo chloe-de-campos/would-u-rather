d4d15bdee42aae2e01f45c1b1d9ad36cd444525b
chloe de campos <chloe_de_campos@brown.edu>
2024-06-07 12:46:18 -0400

    reroute

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {  HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Game from './components/Game';
 import Statistics from './components/Statistics';
 import './App.css';
